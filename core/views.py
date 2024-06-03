b1d2f8c870c63cae2105faa97fb151e6bdfc1e35
Rutuja Shinde <shinderutuja-cmpn@atharvacoe.ac.in>
2024-06-03 10:11:03 +0530

    Update views.py
    h

--- core/views.py
+++ core/views.py
@@ -10,52 +10,6 @@ import random
 
 # Create your views here.
 
-# @login_required(login_url='signin')
-# def index(request):
-    #     user_object=User.objects.get(username=request.user.username)
-    #     user_profile=Profile.objects.get(user=user_object)
-
-    #     user_following_list = []
-    #     feed = []
-
-    #     user_following = FollowersCount.objects.filter(follower=request.user.username)
-
-    #     for users in user_following:
-    #         user_following_list.append(users.user)
-
-    #     for usernames in user_following_list:
-    #         feed_lists = Post.objects.filter(user=usernames)
-    #         feed.append(feed_lists)    
-
-    #     feed_list=list(chain(*feed))
-
-    # # user suggestion starts
-    #     all_users = User.objects.all()
-    #     user_following_all = []
-
-    #     for user in user_following:
-    #        user_list = User.objects.get(username=user.user)
-    #        user_following_all.append(user_list)
-    
-    #     new_suggestions_list = [x for x in list(all_users) if (x not in list(user_following_all))]
-    #     current_user = User.objects.filter(username=request.user.username)
-    #     final_suggestions_list = [x for x in list(new_suggestions_list) if ( x not in list(current_user))]
-    #     random.shuffle(final_suggestions_list)
-
-    #     username_profile = []
-    #     username_profile_list = []
-
-    #     for users in final_suggestions_list:
-    #       username_profile.append(users.id)
-
-    #     for ids in username_profile:
-    #       profile_lists = Profile.objects.filter(id_user=ids)
-    #       username_profile_list.append(profile_lists)
-
-    #       suggestions_username_profile_list = list(chain(*username_profile_list))
-    #     # posts=Post.objects.all()
-    #     return render(request, 'index.html',{'user_profile':user_profile,'posts':feed_list})
-
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
